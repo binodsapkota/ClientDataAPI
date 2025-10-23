2a0683c4948a2a9ae6e7af78ba06905e8c650c57
binodsapkota <binodsapkota96@gmail.com>
2025-10-23 19:44:27 -0400

    added azure update for auth

--- ClientDataAPI/Program.cs
+++ ClientDataAPI/Program.cs
@@ -1,21 +1,27 @@
 using ClientDataAPI.Data;
-using ClientDataAPI.Services;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Identity;
+
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services
 builder.Services.AddControllers();
 builder.Services.AddDbContext<ClientDbContext>(options =>
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
-builder.Services.AddScoped<ClientService>();
-builder.Services.AddScoped<AuthService>();
-builder.Services.AddScoped<SecretManager>();
+
+// Add Azure AD B2C Authentication
+builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
+    .AddMicrosoftIdentityWebApi(builder.Configuration.GetSection("AzureAdB2C"));
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+app.UseAuthentication();
 app.UseAuthorization();
+
 app.MapControllers();
 
 app.Run();
